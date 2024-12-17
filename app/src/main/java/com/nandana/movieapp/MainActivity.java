3259183431701d719988b2294e52a21406c99326
nandana-sivan <nandanasivan.02@gmail.com>
2024-12-17 12:38:15 +0530

    menu pg created

--- app/src/main/java/com/nandana/movieapp/MainActivity.java
+++ app/src/main/java/com/nandana/movieapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.nandana.movieapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +14,30 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText ed1,ed2;
+    Button b1;
+    String getUse,getPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        ed1=(EditText) findViewById(R.id.un);
+        ed2=(EditText) findViewById(R.id.pw);
+        b1=(Button) findViewById(R.id.login);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getUse=ed1.getText().toString();
+                getPass=ed2.getText().toString();
+                Intent ob=new Intent(getApplicationContext(), MainActivity2.class);
+                if(getUse.equals("theatre")&& getPass.equals("12345"))
+                    startActivity(ob);
+                else
+                    Toast.makeText(getApplicationContext(),"invalid",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
